124e61115dc64b9604069e3326a159cad6908ace
Hermann Roitinger <hermann.roitinger@dccs.at>
2022-02-06 16:58:37 +0100

    First Test

--- cmd/main.go
+++ cmd/main.go
@@ -2,25 +2,31 @@ package main
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
 
 type team struct {
-	ID         int `json:"id"`
+	Id         int `json:"id"`
 	Developers int `json:"developers"`
 }
 
-var teams = []team{}
+var teams map[int]team
 
 type project struct {
-	ID          int `json:"id"`
+	Id          int `json:"id"`
 	Devs_needed int `json:"devs_needed"`
+	Team_id     int
+	Index       int
 }
 
-var projects = []project{}
+var projects map[int]project
 
 func main() {
+	teams = make(map[int]team)
+	projects = make(map[int]project)
+
 	router := gin.Default()
 	router.GET("/status", getStatus)
 	router.PUT("/teams", putTeams)
@@ -35,34 +41,75 @@ func getStatus(c *gin.Context) {
 }
 
 func putTeams(c *gin.Context) {
-	if err := c.BindJSON(&teams); err != nil {
+	var newTeams []team
+	if err := c.BindJSON(&newTeams); err != nil {
 		return
+	}
+	tc := 0
+	for _, ct := range newTeams {
+		tc++
+		teams[tc] = ct
 	}
 	c.IndentedJSON(http.StatusOK, teams)
 }
 
 func postProject(c *gin.Context) {
 	var newProject project
-
 	if err := c.BindJSON(&newProject); err != nil {
 		return
 	}
-
-	projects = append(projects, newProject)
-
-	c.IndentedJSON(http.StatusOK, newProject)
+	projectFound := false
+	for _, p := range projects {
+		if p.Id == newProject.Id {
+			p.Devs_needed = newProject.Devs_needed
+			projectFound = true
+		}
+	}
+	for _, t := range teams {
+		if t.Developers == newProject.Devs_needed {
+			newProject.Team_id = t.Id
+		}
+	}
+	if !projectFound {
+		newProject.Index = len(projects) + 1
+		projects[newProject.Index] = newProject
+	}
+	c.IndentedJSON(http.StatusOK, nil)
 }
 
 func postCompleted(c *gin.Context) {
-
-	id := c.Query("id")
-
-	c.IndentedJSON(http.StatusOK, id)
+	id, err := strconv.Atoi(c.Query("id"))
+	if err != nil {
+		c.IndentedJSON(http.StatusBadRequest, "wrong id")
+		return
+	}
+	// search for a project
+	for _, p := range projects {
+		if p.Id == id {
+			delete(projects, p.Index)
+			c.IndentedJSON(http.StatusOK, p)
+			return
+		}
+	}
+	c.IndentedJSON(http.StatusNotFound, id)
 }
 
 func postAssigned(c *gin.Context) {
-
-	id := c.Query("id")
-
-	c.IndentedJSON(http.StatusOK, id)
+	id, err := strconv.Atoi(c.Query("id"))
+	if err != nil {
+		c.IndentedJSON(http.StatusBadRequest, "wrong id")
+		return
+	}
+	// search for a project
+	for _, p := range projects {
+		if p.Id == id {
+			if p.Team_id == 0 {
+				c.IndentedJSON(http.StatusNoContent, nil)
+				return
+			}
+			c.IndentedJSON(http.StatusOK, p)
+			return
+		}
+	}
+	c.IndentedJSON(http.StatusNotFound, id)
 }
